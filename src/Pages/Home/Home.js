0ae5e57dfc44cab06589071efa5c5a1da378ccf4
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-02-10 10:06:47 -0800

    workin

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -18,7 +18,7 @@ const Home = () => (
      <h3>I am a former CEO and founder of a transport company with almost one hundred employees. 
        I was born in central Europe where studied Psychology for two years and later I decided to get a Bachelor's degree in Business Administration. 
        I am fluent in three languages. I can speak in one of the oldest Proto-Indo-European language as well as Russian. I can write and read in Cyrillic script and I am fluent in English. 
-       I moved to the United States in 2017. Since I got into tech and coding that is why in June of 2020 I decided to sell my company and started UCLA extension for full-stack web developer 
+       I moved to the United States in 2017. Since then I got into tech and coding that is why in June of 2020 I decided to sell my company and started UCLA extension for full-stack web developer 
        boot camp which I graduated in February of 2021. I committed my all-time to get better learning front and back end web development but that's not it. 
        Rest of my free time I spent on the coding academy and other online courses learning other coding languages. 
        I know that you see Junior developer but with my commitment and responsibility you will get the best employee you have ever seen!</h3>
--- src/components/LJ/Lawrence.js
+++ src/components/LJ/Lawrence.js
@@ -4,17 +4,23 @@ import styled from 'styled-components';
 
 const TypeStyle = styled.p`
   font-size:45px;
+  fontFamily: 'Montserrat', sans-serif;
+  color:  #999C9D;
   top: 5%;
-  left: 43%;
+  left:43%;
   bottom: 20%;
-  white-space: nowrap;
-  overflow: hidden;
+  opacity: 0.8;
+  white-space: pre;
+  overflow: auto;
   width: 3.9em;
   position: relative; 
   animation: type 1s steps(10, end); 
   @keyframes type{ from { width: 0 } 
-
   } 
+
+
+ 
+
 `
 const Lawrence = () => (
 
