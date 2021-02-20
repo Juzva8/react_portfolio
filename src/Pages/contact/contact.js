f63fd28540cf9e233d1f05176c095d111d6a5030
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-02-19 17:39:48 -0800

    should be good for now

--- src/Pages/contact/contact.js
+++ src/Pages/contact/contact.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import './contact.css';
 import ContactUs from '../../components/contactForm/ContactUs'
-
-
-
-
 
 
 function Contact() {
   return (
-    <div> 
-  
+    <container> 
+      <h4> Email:     
+       <a>
+       laurynas.juzva@gmail.com</a> 
+       </h4>
+       <h4> Phone Number:     
+       <a>
+       ‪(951) 254-7315‬ </a> 
+       </h4>
+      
 <ContactUs />
-    </div>
+    </container>
 
   );
 }
