67f670925645147a0315351bcb7b5ebe09b5225d
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-03-04 09:19:37 -0800

    few fixes

--- src/Pages/contact/contact.js
+++ src/Pages/contact/contact.js
@@ -6,13 +6,13 @@ import ContactUs from '../../components/contactForm/ContactUs'
 function Contact() {
   return (
     <container> 
-      <h4> <strong>Email:</strong>
-       <a>
-       laurynas.juzva@gmail.com</a> 
-       </h4>
-       <h4> <strong>Phone Number:</strong> 
-       <a>
-       ‪(951) 254-7315‬ </a> 
+      <h4> <strong>Email:  </strong>
+       <>
+        laurynas.juzva@gmail.com</> 
+              </h4>
+       <h4> <strong>Phone Number:  </strong> 
+       <>
+        ‪(951) 254-7315‬ </> 
        </h4>
       
 <ContactUs />
