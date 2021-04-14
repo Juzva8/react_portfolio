f80ab04e59e2dd30bccfc67d18fc267ccc46ee26
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-04-14 10:33:01 -0700

    yes

--- src/Pages/contact/contact.js
+++ src/Pages/contact/contact.js
@@ -6,14 +6,7 @@ import ContactUs from '../../components/contactForm/ContactUs'
 function Contact() {
   return (
     <container> 
-      <h4> <strong>Email:  </strong>
-       <>
-        laurynas.juzva@gmail.com</> 
-              </h4>
-       <h4> <strong>Phone Number:  </strong> 
-       <>
-        ‪(951) 254-7315‬ </> 
-       </h4>
+ 
       
 <ContactUs />
     </container>
